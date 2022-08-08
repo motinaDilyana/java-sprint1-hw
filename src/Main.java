ba3c3ff9ba608256955f24b7c06da64e15eb3d94
dilyana.motina <d.motina>
2022-08-08 21:44:31 +0300

    исправлена обработка вводимых данных, работа в Main.java разбита на методы, переименованы геттеры

--- src/Main.java
+++ src/Main.java
@@ -9,31 +9,10 @@ public class Main {
         //пока пользователь не вышел из приложения, исполняем команды и печатаем меню
         while (userInput != 0) {
             try {
-                if (userInput == 1) {
-                    System.out.println("Введите номер месяца с 0");
-                    int month = scanner.nextInt();
-                    System.out.println("Введите день");
-                    int day = scanner.nextInt();
-                    System.out.println("Введите количество пройденных шагов");
-                    int stepsCount = scanner.nextInt();
-                    //Сохраняем количество пройденных шагов
-                    stepTracker.saveStepsCount(month, day, stepsCount);
-                } else if (userInput == 2) {
-                    System.out.println("Введите номер месяца с 0");
-                    int month = scanner.nextInt();
-                    //выводим статистику
-                    stepTracker.printStatByMonth(month);
-                } else if (userInput == 3) {
-                    System.out.println("Введите новую цель шагов");
-                    //изменяем дневную цель шагов
-                    stepTracker.changeDailyStepGoal(scanner.nextInt());
-                } else {
-                    System.out.println("Такой команды не существует. Выберите что-то другое");
-                }
+                performInputInstructions(userInput, scanner, stepTracker);
             } catch (IllegalArgumentException exception) {
                 System.out.println(exception.getMessage());
             }
-
             printMenu();
             userInput = scanner.nextInt();
         }
@@ -45,5 +24,29 @@ public class Main {
         System.out.println("2-Напечатать статистику за определённый месяц");
         System.out.println("3-Изменить цель по количеству шагов в день");
         System.out.println("0-Выйти из приложения");
+    }
+
+    public static void performInputInstructions(int userInput, Scanner scanner, StepTracker stepTracker) {
+        if (userInput == 1) {
+            int month = readUserInput("Введите номер месяца с 0", scanner);
+            int day = readUserInput("Введите день начиная с 0", scanner);
+            int stepsCount = readUserInput("Введите количество пройденных шагов", scanner);
+            //Сохраняем количество пройденных шагов
+            stepTracker.saveStepsCount(month, day, stepsCount);
+        } else if (userInput == 2) {
+            int month = readUserInput("Введите номер месяца с 0", scanner);
+            //выводим статистику
+            stepTracker.printStatByMonth(month);
+        } else if (userInput == 3) {
+            //изменяем дневную цель шагов
+            stepTracker.changeDailyStepGoal(readUserInput("Введите новую цель шагов", scanner));
+        } else {
+            System.out.println("Такой команды не существует. Выберите что-то другое");
+        }
+    }
+
+    public static int readUserInput(String message, Scanner scanner) {
+        System.out.println(message);
+        return scanner.nextInt();
     }
 }
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -26,38 +26,41 @@ public class StepTracker {
         if(steps < 0) {
             throw new IllegalArgumentException("Значение не может быть отрицательным");
         }
-        if(month < 0 || month > 12) {
-            throw new IllegalArgumentException("Месяц не может быть меньше нуля или больше 12");
+        if(month < 0 || month > 11) {
+            throw new IllegalArgumentException("Месяц не может быть меньше нуля или больше 11");
         }
-        if(day < 1 || day > 30) {
-            throw new IllegalArgumentException("День не может быть меньше единицы или больше 30");
+        if(day < 0 || day > 29) {
+            throw new IllegalArgumentException("День не может быть меньше нуля или больше 29");
         }
-        monthToData[month].dailyStepsCount[day - 1] = steps;
+        monthToData[month].dailyStepsCount[day] = steps;
     }
 
     void printStatByMonth(int month) {
-        int stepsSum = getStepsSum(monthToData[month].dailyStepsCount);
+        if(month < 0 || month > 11 ) {
+            throw new IllegalArgumentException("Месяц не может быть меньше нуля или больше 11");
+        }
+        int stepsSum = calculateStepsSum(monthToData[month].dailyStepsCount);
         int[] monthData = monthToData[month].dailyStepsCount;
         //выводим статистику
         printDailyStat(monthData);
         System.out.println();
         System.out.println("Пройдено шагов за месяц: " + stepsSum);
         System.out.println("Среднее кол-во шагов в этом месяце " + stepsSum/monthData.length);
-        System.out.println("Максимальное пройденное количество шагов в месяце " + getMaxSteps(monthData));
+        System.out.println("Максимальное пройденное количество шагов в месяце " + calculateMaxSteps(monthData));
         //конвертируем при помощи класса Converter шаги в пройденную дистанцию
         System.out.println("Пройденная дистанция (в км) " + Converter.convertDistance(stepsSum));
         //конвертируем при помощи класса Converter шаги в килокалории
         System.out.println("Количество сожжённых килокалорий " + Converter.convertToCall(stepsSum));
-        System.out.println("Лучшая серия " + getBestDaysSeries(monthData));
+        System.out.println("Лучшая серия " + calculateBestDaysSeries(monthData));
     }
 
     void printDailyStat(int[] monthData) {
-        for (int i = 1; i<monthData.length; i++) {
-            System.out.print(i + " день: " + monthData[i -1] + ", ");
+        for (int i = 0; i<monthData.length; i++) {
+            System.out.print(i + 1 + " день: " + monthData[i] + ", ");
         }
     }
 
-    public int getStepsSum(int[] monthData) {
+    public int calculateStepsSum(int[] monthData) {
         int stepsSum = 0;
         for (int monthDatum : monthData) {
             stepsSum = stepsSum + monthDatum;
@@ -66,7 +69,7 @@ public class StepTracker {
         return stepsSum;
     }
 
-    public int getMaxSteps(int[] monthData) {
+    public int calculateMaxSteps(int[] monthData) {
         int max = 0;
         for (int i = 1; i<monthData.length; i++) {
             if(monthData[i-1] > max) {
@@ -77,7 +80,7 @@ public class StepTracker {
         return max;
     }
 
-    public int getBestDaysSeries(int[] monthData) {
+    public int calculateBestDaysSeries(int[] monthData) {
         int daysCount = 0;
         int maxDaysCount = 0;
         for (int monthDatum : monthData) {
